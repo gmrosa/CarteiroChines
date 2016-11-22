e5b192f934c68fde9d62a33ea6b2ed3a6d113623
gmrosa <gmurilorosa@gmail.com>
2016-11-22 02:28:20 -0200

    v2

--- CarteiroChines.java
+++ CarteiroChines.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,21 +8,32 @@ import java.util.Map;
  * @author Guilherme Murilo da Rosa & Plamedi Luzolo Lusembo
  */
 public class CarteiroChines {
-	public static int INFINITO = Integer.MAX_VALUE;
+	private static int INFINITO = Integer.MAX_VALUE;
 	private int tamanho;
-	private Map<Aresta, CaminhoDijkstra> dijkstras;
+	private List<Character> verticesGrauImpar = new LinkedList<>();
+	private Map<Aresta, CaminhoDijkstra> dijkstras = new LinkedHashMap<>();
 	private Map<Character, List<Aresta>> grafo = new LinkedHashMap<>();
 	private int[][] matriz;
 
 	public static void main(String[] args) {
 		// XXX Usar infinito quando não houver ligação entre vertices
+		// int[][] matriz = { //
+		// { 0, 9, 10, INFINITO, INFINITO, 6 }, //
+		// { 9, 0, 5, INFINITO, INFINITO, INFINITO }, //
+		// { 10, 5, 0, 5, INFINITO, 14 }, //
+		// { INFINITO, INFINITO, 5, 0, 3, 8 }, //
+		// { INFINITO, INFINITO, INFINITO, 3, 0, 4 }, //
+		// { 6, INFINITO, 14, 8, 4, 0 }, //
+		// };
 		int[][] matriz = { //
-				{ 0, 9, 10, INFINITO, INFINITO, 6 }, //
-				{ 9, 0, 5, INFINITO, INFINITO, INFINITO }, //
-				{ 10, 5, 0, 5, INFINITO, 14 }, //
-				{ INFINITO, INFINITO, 5, 0, 3, 8 }, //
-				{ INFINITO, INFINITO, INFINITO, 3, 0, 4 }, //
-				{ 6, INFINITO, 14, 8, 4, 0 }, //
+				{ 0, 12, 4, INFINITO, INFINITO, INFINITO, 3, INFINITO }, //
+				{ 12, 0, 6, 10, INFINITO, 19, 3, INFINITO }, //
+				{ 4, 6, 0, INFINITO, INFINITO, INFINITO, INFINITO, INFINITO }, //
+				{ INFINITO, 10, INFINITO, 0, INFINITO, 7, INFINITO, INFINITO }, //
+				{ INFINITO, INFINITO, INFINITO, INFINITO, 0, 8, 6, 2 }, //
+				{ INFINITO, 19, INFINITO, 7, 8, 0, 7, 3 }, //
+				{ 3, 3, INFINITO, INFINITO, 6, 7, 0, INFINITO }, //
+				{ INFINITO, INFINITO, INFINITO, INFINITO, 2, 3, INFINITO, 0 }, //
 		};
 
 		CarteiroChines carteiroChines = new CarteiroChines(matriz);
@@ -29,7 +41,7 @@ public class CarteiroChines {
 		System.out.println();
 	}
 
-	CarteiroChines(int[][] matriz) {
+	private CarteiroChines(int[][] matriz) {
 		if (matriz != null && matriz.length > 0) {
 			tamanho = matriz.length;
 			if (tamanho == matriz[0].length) {
@@ -61,38 +73,71 @@ public class CarteiroChines {
 
 	private void executar() {
 		executarDijkstraParaVerticesDeGrauImpar();
+		duplicarCaminhosDeMenorCusto();
 		eulerizarGrafo();
 	}
 
 	private void executarDijkstraParaVerticesDeGrauImpar() {
-		dijkstras = new LinkedHashMap<>();
+		List<Character> verticesGrauImpar = getVerticesGrauImpar();
+		if (ehPar(verticesGrauImpar.size())) {
+			while (!verticesGrauImpar.isEmpty()) {
+				Character verticeOrigem = verticesGrauImpar.remove(0);
+				if (!verticesGrauImpar.isEmpty()) {
+					Dijkstra dijkstra = new Dijkstra(tamanho, matriz);
+					dijkstra.executar(verticeOrigem);
+					for (Character verticeDestino : verticesGrauImpar) {
+						CaminhoDijkstra caminhoDijkstra = dijkstra.getCaminhoMinimo(verticeDestino);
+						int custo = caminhoDijkstra.custoTotal;
+						Aresta aresta = new Aresta(verticeOrigem, verticeDestino, custo);
+						dijkstras.put(aresta, caminhoDijkstra);
+					}
+				}
+			}
+		} else {
+			throw new IllegalArgumentException("A quantidade de vértices de grau impar deve ser par.");
+		}
+	}
+
+	private List<Character> getVerticesGrauImpar() {
+		this.verticesGrauImpar = new ArrayList<>();
 		List<Character> verticesGrauImpar = new ArrayList<>();
 		for (Map.Entry<Character, List<Aresta>> entry : grafo.entrySet()) {
 			int grau = entry.getValue().size();
-			if (temGrauImpar(grau)) {
+			boolean temGrauImpar = !ehPar(grau);
+			if (temGrauImpar) {
 				char vertice = entry.getKey();
 				verticesGrauImpar.add(vertice);
 			}
 		}
-
-		for (Character verticeOrigem : verticesGrauImpar) {
-			List<Character> pilhaVertices = new ArrayList<>();
-			pilhaVertices.addAll(verticesGrauImpar);
-			pilhaVertices.remove(verticeOrigem);
-			Dijkstra dijkstra = new Dijkstra(tamanho, matriz);
-			dijkstra.executar(verticeOrigem);
-			for (Character verticeDestino : pilhaVertices) {
-				CaminhoDijkstra caminhoDijkstra = dijkstra.getCaminhoMinimo(verticeDestino);
-				int custo = caminhoDijkstra.custoTotal;
-				Aresta aresta = new Aresta(verticeOrigem, verticeDestino, custo);
-				dijkstras.put(aresta, caminhoDijkstra);
-			}
-		}
-
+		return verticesGrauImpar;
 	}
 
-	private boolean temGrauImpar(int grau) {
-		return grau % 2 != 0;
+	private boolean ehPar(int grau) {
+		return grau % 2 == 0;
+	}
+
+	private void duplicarCaminhosDeMenorCusto() {
+		Map<Aresta, CaminhoDijkstra> dijkstrasTemp = new LinkedHashMap<>(dijkstras);
+		List<ConjuntoDeCaminhos> caminhos = new ArrayList<>();
+
+		int quantidadeMaximaCombinacoes = verticesGrauImpar.size() - 1;
+		List<Character> verticesGrauImparTemp = new LinkedList<>();
+
+		while (caminhos.size() < quantidadeMaximaCombinacoes) {
+			verticesGrauImparTemp.addAll(verticesGrauImparTemp);
+			Character vertice = verticesGrauImparTemp.remove(0);
+			ConjuntoDeCaminhos conjuntoDeCaminhos = new ConjuntoDeCaminhos();
+			while (conjuntoDeCaminhos.conjunto.size() < verticesGrauImparTemp.size() / 2) {
+				for (Map.Entry<Aresta, CaminhoDijkstra> entry : dijkstrasTemp.entrySet()) {
+					Aresta aresta = entry.getKey();
+					if (aresta.verticeOrigem == vertice) {
+					
+					}
+				}
+			}
+
+		}
+
 	}
 
 	private void eulerizarGrafo() {
@@ -100,78 +145,86 @@ public class CarteiroChines {
 		// }
 	}
 
-	static class CaminhoDijkstra {
-		private List<Aresta> caminho = new ArrayList<>();
+	private static class ConjuntoDeCaminhos {
+		private List<CaminhoDijkstra> conjunto = new LinkedList<>();
 		private int custoTotal;
 
-		public CaminhoDijkstra(int custoTotal) {
-			this.custoTotal = custoTotal;
-		}
-
-		public void addAresta(Aresta aresta) {
-			caminho.add(aresta);
-		}
-
-		public int getCustoTotal() {
-			return custoTotal;
+		private void addCaminho(CaminhoDijkstra caminho) {
+			custoTotal += caminho.custoTotal;
+			conjunto.add(caminho);
 		}
 
 	}
 
-	static class Aresta {
-		private char verticeOrigem;
-		private char verticeDestino;
-		private int custo;
+	private static class CaminhoDijkstra {
+		private List<Aresta> caminho = new ArrayList<>();
+		private int custoTotal;
 
-		public Aresta(char verticeOrigem, char verticeDestino, int custo) {
-			this.verticeOrigem = verticeOrigem;
-			this.verticeDestino = verticeDestino;
-			this.custo = custo;
+		private CaminhoDijkstra(int custoTotal) {
+			this.custoTotal = custoTotal;
 		}
 
-		public Aresta(char verticeOrigem, char verticeDestino) {
-			this.verticeOrigem = verticeOrigem;
-			this.verticeDestino = verticeDestino;
-		}
-
-		public Aresta(char verticeDestino, int custo) {
-			this.verticeDestino = verticeDestino;
-			this.custo = custo;
-		}
-
-		public char getVerticeOrigem() {
-			return verticeOrigem;
-		}
-
-		public char getVerticeDestino() {
-			return verticeDestino;
-		}
-
-		public int getCusto() {
-			return custo;
+		private void addAresta(Aresta aresta) {
+			caminho.add(aresta);
 		}
 
 		@Override
 		public String toString() {
+			return "CaminhoDijkstra [caminho=" + caminho + ", custoTotal=" + custoTotal + "]";
+		}
+
+	}
+
+	private static class Aresta {
+		private char verticeOrigem;
+		private char verticeDestino;
+		private int custo;
+
+		private Aresta(char verticeOrigem, char verticeDestino, int custo) {
+			this.verticeOrigem = verticeOrigem;
+			this.verticeDestino = verticeDestino;
+			this.custo = custo;
+		}
+
+		private Aresta(char verticeOrigem, char verticeDestino) {
+			this.verticeOrigem = verticeOrigem;
+			this.verticeDestino = verticeDestino;
+		}
+
+		private Aresta(char verticeDestino, int custo) {
+			this.verticeDestino = verticeDestino;
+			this.custo = custo;
+		}
+
+		/**
+		 * Somente para debug
+		 */
+		@Override
+		public String toString() {
+			if (verticeOrigem > 0) {
+				if (custo > 0) {
+					return "[verticeOrigem=" + verticeOrigem + ", verticeDestino=" + verticeDestino + ", custo=" + custo + "]";
+				}
+				return "[verticeOrigem=" + verticeOrigem + ", verticeDestino=" + verticeDestino + "]";
+			}
 			return "[verticeDestino=" + verticeDestino + ", custo=" + custo + "]";
 		}
 
 	}
 
-	static class Dijkstra {
-
+	private static class Dijkstra {
 		private int tamanho;
 		private int posicaoOrigem;
 		private int[][] matriz;
 		private String dijkstra[][];
 
-		Dijkstra(int tamanho, int[][] matriz) {
+		private Dijkstra(int tamanho, int[][] matriz) {
 			this.tamanho = tamanho;
 			this.matriz = matriz;
 			this.dijkstra = new String[3][tamanho];
 		}
 
-		String[][] executar(char verticeOrigem) {
+		private String[][] executar(char verticeOrigem) {
 			posicaoOrigem = Vertice.getPosicao(verticeOrigem);
 			inicializarDijkstra();
 			return gerarDijkstra();
@@ -181,7 +234,6 @@ public class CarteiroChines {
 			for (int coluna = 0; coluna < tamanho; coluna++) {
 				if (posicaoOrigem == coluna) {
 					dijkstra[0][coluna] = "0";
-					dijkstra[1][coluna] = "nil";
 				} else {
 					dijkstra[0][coluna] = String.valueOf(INFINITO);
 				}
@@ -198,7 +250,7 @@ public class CarteiroChines {
 		}
 
 		private int getVerticeMenorCusto() {
-			int verticeMenorCusto = -1;
+			int verticeMenorCusto = 0;
 			int menorCustoDisponivel = INFINITO;
 			for (int coluna = 0; coluna < tamanho; coluna++) {
 				if (dijkstra[2][coluna] == null) {
@@ -233,23 +285,24 @@ public class CarteiroChines {
 			return dijkstra;
 		}
 
-		CaminhoDijkstra getCaminhoMinimo(char verticeDestino) {
-			int posicaoAtual = posicaoOrigem;
-			int posicaoDestino = Vertice.getPosicao(verticeDestino);
-			int custoTotal = Integer.parseInt(dijkstra[0][posicaoDestino]);
+		private CaminhoDijkstra getCaminhoMinimo(char verticeDestino) {
+			int posicaoAtual = Vertice.getPosicao(verticeDestino);
+			int custoTotal = Integer.parseInt(dijkstra[0][posicaoAtual]);
 
 			CaminhoDijkstra caminhoDijkstra = new CaminhoDijkstra(custoTotal);
 
-			while (!dijkstra[1][posicaoAtual].equals(String.valueOf(verticeDestino))) {
-				char verticeOrigem = Vertice.getNome(posicaoAtual);
-				posicaoAtual = Vertice.getPosicao(dijkstra[1][Vertice.getPosicao(verticeOrigem)].charAt(0));
-				char verticeDestino2 = Vertice.getNome(posicaoAtual);
-				Aresta aresta = new Aresta(verticeOrigem, verticeDestino2);
+			while (dijkstra[1][posicaoAtual] != null) {
+				char verticeTemp = Vertice.getNome(posicaoAtual);
+				posicaoAtual = Vertice.getPosicao(dijkstra[1][Vertice.getPosicao(verticeTemp)].charAt(0));
+				Aresta aresta = new Aresta(verticeTemp, Vertice.getNome(posicaoAtual));
 				caminhoDijkstra.addAresta(aresta);
 			}
 			return caminhoDijkstra;
 		}
 
+		/**
+		 * Somente para debug
+		 */
 		@Override
 		public String toString() {
 			String s = "";
@@ -268,7 +321,7 @@ public class CarteiroChines {
 
 	}
 
-	static class Vertice {
+	private static class Vertice {
 
 		static char getNome(int posicao) {
 			return ((char) (65 + posicao));
